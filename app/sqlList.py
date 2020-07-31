822b269e87fa5d35f8349f15a9e5b2fd5a1c0aa8
七戒 <1123093510@qq.com>
2020-07-31 10:58:15 +0800

    9991

--- app/sqlList.py
+++ app/sqlList.py
@@ -5,3 +5,6 @@ def testmsg():
     print(type(a))
     return {"data":a}
 
+def push():
+    INSERT INTO article (id,title,create_user,category,description,content,create_time) VALUES (REPLACE(UUID(),"-",""),"sss","sss",1,"description","content","2020-07-31 10:40:12");
+    INSERT INTO article_list (id,article_id,title,create_user,category,description,create_time) VALUES(REPLACE(UUID(),"-",""),"hhh","sss","sss",1,"description","2020-07-31 10:40:12");
